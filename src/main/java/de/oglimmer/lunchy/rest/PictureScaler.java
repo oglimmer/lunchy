f1c7aef3d82140cb5962941b925f52f224f863ad
Oli Zimpasser <mail@olikurt.de>
2014-08-14 23:57:18 +0200

    Respect orientation in pictures (as iPhones does)

--- src/main/java/de/oglimmer/lunchy/rest/PictureScaler.java
+++ src/main/java/de/oglimmer/lunchy/rest/PictureScaler.java
@@ -1,12 +1,19 @@
 package de.oglimmer.lunchy.rest;
 
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+
+import org.imgscalr.Scalr;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
 
 import de.oglimmer.lunchy.services.FileServices;
 import de.oglimmer.lunchy.services.LunchyProperties;
@@ -77,11 +84,59 @@ public class PictureScaler {
 	}
 
 	private void createShrinkedImage(int width, int height) throws IOException {
-		double scaleX = (double) width / imageWidth;
-		double scaleY = (double) height / imageHeight;
-		AffineTransform scaleTransform = AffineTransform.getScaleInstance(scaleX, scaleY);
-		AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
-		scaledImage = bilinearScaleOp.filter(originalImage, new BufferedImage(width, height, originalImage.getType()));
+		BufferedImage tmpImg = fixRotation();
+		scaledImage = Scalr.resize(tmpImg, Scalr.Method.QUALITY, width, height);
+	}
+
+	/**
+	 * 'Modern' devices store a picture in an arbitrary orientation and add the meta data (EXIF) to tell you how you need to look at it.
+	 * This method moves the pictures into the right orientation.
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private BufferedImage fixRotation() throws IOException {
+		BufferedImage tmpImg = originalImage;
+		// http://sylvana.net/jpegcrop/exif_orientation.html
+		switch (getOrientation(originalFile)) {
+		case 1:
+			break;
+		case 2:
+			tmpImg = Scalr.rotate(tmpImg, Scalr.Rotation.FLIP_HORZ);
+			break;
+		case 3:
+			tmpImg = Scalr.rotate(tmpImg, Scalr.Rotation.CW_180);
+			break;
+		case 4:
+			BufferedImage step1ImgA = Scalr.rotate(tmpImg, Scalr.Rotation.CW_180);
+			tmpImg = Scalr.rotate(step1ImgA, Scalr.Rotation.FLIP_HORZ);
+			break;
+		case 5:
+			BufferedImage step1ImgB = Scalr.rotate(tmpImg, Scalr.Rotation.CW_90);
+			tmpImg = Scalr.rotate(step1ImgB, Scalr.Rotation.FLIP_HORZ);
+			break;
+		case 6:
+			tmpImg = Scalr.rotate(tmpImg, Scalr.Rotation.CW_90);
+			break;
+		case 7:
+			BufferedImage step1ImgC = Scalr.rotate(tmpImg, Scalr.Rotation.CW_270);
+			tmpImg = Scalr.rotate(step1ImgC, Scalr.Rotation.FLIP_HORZ);
+			break;
+		case 8:
+			tmpImg = Scalr.rotate(tmpImg, Scalr.Rotation.CW_270);
+			break;
+		}
+		return tmpImg;
+	}
+
+	private static int getOrientation(File imageFile) throws IOException {
+		try {
+			Metadata metadata = ImageMetadataReader.readMetadata(imageFile);
+			Directory directory = metadata.getDirectory(ExifIFD0Directory.class);
+			return directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+		} catch (MetadataException | ImageProcessingException me) {
+			return 1;
+		}
 	}
 
 }
