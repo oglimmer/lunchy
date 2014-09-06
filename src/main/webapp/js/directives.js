25b5b6d7f5e8007ac908b6606a4c9d94fa293c90
Oli Zimpasser <mail@olikurt.de>
2014-09-07 01:46:07 +0200

    Added "enter" key support for login dialog

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -121,4 +121,18 @@ directive('lyDynamicname', function() {
 	        }	        
 		}
     };
+}).
+directive('lyEnter', function() {
+    return {
+        link: function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.lyEnter, {'event': event});
+                    });
+                    event.preventDefault();
+                }
+            });
+        }
+    };
 });
