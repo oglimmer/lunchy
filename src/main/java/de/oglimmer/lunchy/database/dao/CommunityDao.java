47c237584fd37ce359fcb612762b838fccb63a48
Oli Zimpasser <mail@olikurt.de>
2014-09-13 22:05:44 +0200

    Added cache for Community Dao

--- src/main/java/de/oglimmer/lunchy/database/dao/CommunityDao.java
+++ src/main/java/de/oglimmer/lunchy/database/dao/CommunityDao.java
@@ -2,13 +2,35 @@ package de.oglimmer.lunchy.database.dao;
 
 import static de.oglimmer.lunchy.database.dao.DaoBackend.DB;
 import static de.oglimmer.lunchy.database.generated.tables.Communities.COMMUNITIES;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 import de.oglimmer.lunchy.database.generated.tables.records.CommunitiesRecord;
 
 public enum CommunityDao {
 	INSTANCE;
 
+	private LoadingCache<String, CommunitiesRecord> userRecordCache = CacheBuilder
+			.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES)
+			.build(new CacheLoader<String, CommunitiesRecord>() {
+				public CommunitiesRecord load(String domain) {
+					return DB.fetchOn(COMMUNITIES,
+							COMMUNITIES.DOMAIN.equalIgnoreCase(domain));
+				}
+			});
+
 	public CommunitiesRecord getByDomain(String domain) {
-		return DB.fetchOn(COMMUNITIES, COMMUNITIES.DOMAIN.equalIgnoreCase(domain));
+		try {
+			return userRecordCache.get(domain);
+		} catch (ExecutionException e) {
+			return DB.fetchOn(COMMUNITIES,
+					COMMUNITIES.DOMAIN.equalIgnoreCase(domain));
+		}
 	}
 
 	public CommunitiesRecord getById(int id) {
