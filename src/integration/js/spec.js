59220bb0a92f51825d78d4cf340a087b48000a85
Oli Zimpasser <mail@olikurt.de>
2014-10-04 16:55:09 +0200

    Added first e2e tests

--- src/integration/js/spec.js
+++ src/integration/js/spec.js
@@ -1,24 +1,66 @@
 describe('Lunchy', function() {
+	
+	var userEmail = 'test@test.de' + Math.random();
+	
+	it('Create user', function() {
+		
+		browser.get('http://localhost:8080/#/updates');
+		
+		var login = element(by.linkText('Login/Register'));
+		login.click();
 
-	it('contains spec with an expectation', function() {
-		expect(true).toBe(true);
+		var register = element(by.buttonText('Register account'));
+		register.click();
+
+		var email = element(by.model('newUser.email'))
+		email.sendKeys(userEmail);
+		var email = element(by.model('newUser.displayname'))
+		email.sendKeys('Test-User');
+		var pass = element(by.model('newUser.password'))
+		pass.sendKeys('fooba');
+		var pass = element(by.model('newUser.confirm_password'))
+		pass.sendKeys('fooba');
+		element(by.cssContainingText('option', 'Office No.1')).click();
+		
+		element(by.buttonText('Register')).click();
+		
+		element(by.linkText('Log out')).click();
+		
 	});
-
-	it('should have a title', function() {
-				
+	
+	function login() {
 		browser.get('http://localhost:8080/#/updates');
 
 		var login = element(by.linkText('Login/Register'));
 		login.click();
 
 		var email = element(by.model('login.email'))
-		email.sendKeys('test@test.de');
+		email.sendKeys(userEmail);
 		var pass = element(by.model('login.password'))
-		pass.sendKeys('the_pass');
+		pass.sendKeys('fooba');
 
-		element(by.buttonText('Log me in')).click();
-
+		element(by.buttonText('Log me in')).click();		
+	}
+	
+	it('Login / Logout', function() {
+				
+		login();
+		
+		protractor.getInstance().sleep(500);
 		element(by.linkText('Log out')).click();
 
+	});
+	
+	it('Create location', function() {
+		
+		login();
+		
+		element(by.linkText('Add')).click();
+		
+		element(by.model('data.officialName')).sendKeys("First Test Location")
+		element(by.model('data.address')).sendKeys("Alexanderplatz 1")
+		element(by.model('data.city')).sendKeys("Berlin")
+		
+		element(by.buttonText('Save')).click();
 	});
 });
