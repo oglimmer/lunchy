63badb9c971feb160b87553c3a16ed7fe22e1fab
Oli Zimpasser <mail@olikurt.de>
2014-10-04 23:15:04 +0200

    Added expects to e2e-test

--- src/integration/js/spec.js
+++ src/integration/js/spec.js
@@ -1,6 +1,11 @@
+var Chance = require("./chance.js");
+
 describe('Lunchy', function() {
 	
-	var userEmail = 'test@test.de' + Math.random();
+	var chance = new Chance();
+	
+	var userEmail = chance.email();
+	var displayName = chance.name();
 	
 	it('Create user', function() {
 		
@@ -15,7 +20,7 @@ describe('Lunchy', function() {
 		var email = element(by.model('newUser.email'))
 		email.sendKeys(userEmail);
 		var email = element(by.model('newUser.displayname'))
-		email.sendKeys('Test-User');
+		email.sendKeys(displayName);
 		var pass = element(by.model('newUser.password'))
 		pass.sendKeys('fooba');
 		var pass = element(by.model('newUser.confirm_password'))
@@ -26,7 +31,21 @@ describe('Lunchy', function() {
 		
 		element(by.linkText('Log out')).click();
 		
+		browser.get('http://localhost:8080/#/updates');
+		
+		element.all(by.repeater('update in rows')).filter(function(elem, index) {
+			return elem.getText().then(function(text) {
+				return text === displayName+' joined';
+			});
+		}).then(function(filteredList) {
+			expect(filteredList.length).toBe(1);	
+		});
 	});
+	
+	it('Check new user', function() {
+		
+		
+	});	
 	
 	function login() {
 		browser.get('http://localhost:8080/#/updates');
@@ -53,14 +72,26 @@ describe('Lunchy', function() {
 	
 	it('Create location', function() {
 		
+		var locName = chance.word({syllables: 5});
+		
 		login();
 		
 		element(by.linkText('Add')).click();		
 		
-		element(by.model('data.officialName')).sendKeys("First Test Location")
+		element(by.model('data.officialName')).sendKeys(locName)
 		element(by.model('data.address')).sendKeys("Alexanderplatz 1")
 		element(by.model('data.city')).sendKeys("Berlin")
 				
-		element(by.buttonText('Save')).click();		
+		element(by.buttonText('Save')).click();
+		
+		browser.get('http://localhost:8080/#/updates');
+		
+		element.all(by.repeater('update in rows').column('update.text')).filter(function(elem){
+			return elem.getText().then(function(text) {
+				return text === 'New location '+locName+' in Berlin';
+			});
+		}).then(function(filteredList) {
+			expect(filteredList.length).toBe(1);	
+		});
 	});
 });
