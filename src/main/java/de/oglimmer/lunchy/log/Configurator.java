971cd339ee969c3ebd54369160a3e46fde0debea
Oli Zimpasser <oglimmer@gmail.com>
2016-08-20 21:50:41 +0200

    Move to SPI-based slf4j

---
+++ src/main/java/de/oglimmer/lunchy/log/Configurator.java
@@ -0,0 +1,11 @@
+package de.oglimmer.lunchy.log;
+
+import de.oglimmer.utils.BaseConfigurator;
+
+public class Configurator extends BaseConfigurator {
+
+	public Configurator() {
+		super("lunchy");
+	}
+
+}
