029f57cdb6ba15576ec6b3c625a6139f7b754095
Oli Zimpasser <oglimmer@gmail.com>
2016-12-09 23:28:33 +0100

    Fix protractor test

--- src/integration/js/spec.js
+++ src/integration/js/spec.js
@@ -1,3 +1,15 @@
+/* enable to slow down execution 
+var origFn = browser.driver.controlFlow().execute;
+browser.driver.controlFlow().execute = function() {
+  var args = arguments;
+  // queue 100ms wait
+  origFn.call(browser.driver.controlFlow(), function() {
+    return protractor.promise.delayed(100);
+  });
+  return origFn.apply(browser.driver.controlFlow(), args);
+};
+*/
+
 var Chance = require("./chance.js");
 
 describe('Lunchy', function() {
@@ -7,8 +19,18 @@ describe('Lunchy', function() {
 	var userEmail = chance.email();
 	var displayName = chance.name();
 	
-	it('Create user', function() {
-		
+	function logout(followUp) {
+		var logoutLink = element(by.linkText('Log out'));
+		logoutLink.isPresent().then(function(result) {
+			if(result) {
+				logoutLink.click();
+			}
+			followUp();
+		});
+	}
+
+	it('Create user', function() {		
+
 		browser.get('http://localhost:8080/#/updates');
 		
 		var login = element(by.linkText('Login/Register'));
@@ -29,69 +51,74 @@ describe('Lunchy', function() {
 		
 		element(by.buttonText('Register')).click();
 		
-		element(by.linkText('Log out')).click();
+		logout(function () {
+			browser.get('http://localhost:8080/#/updates');
 		
-		browser.get('http://localhost:8080/#/updates');
-		
-		element.all(by.repeater('update in rows')).filter(function(elem, index) {
-			return elem.getText().then(function(text) {
-				return text === displayName+' joined';
+			element.all(by.repeater('update in rows')).filter(function(elem, index) {
+				return elem.getText().then(function(text) {
+					return text === displayName+' joined';
+				});
+			}).then(function(filteredList) {
+				expect(filteredList.length).toBe(1);	
 			});
-		}).then(function(filteredList) {
-			expect(filteredList.length).toBe(1);	
 		});
+			
 	});
 	
-	it('Check new user', function() {
-		
-		
-	});	
 	
-	function login() {
+	function login(followUp) {
 		browser.get('http://localhost:8080/#/updates');
 
-		var login = element(by.linkText('Login/Register'));
-		login.click();
+		logout(function () {
+			var login = element(by.linkText('Login/Register'));
+			login.click();
 
-		var email = element(by.model('login.email'))
-		email.sendKeys(userEmail);
-		var pass = element(by.model('login.password'))
-		pass.sendKeys('fooba');
+			var email = element(by.model('login.email'))
+			email.sendKeys(userEmail);
+			var pass = element(by.model('login.password'))
+			pass.sendKeys('fooba');
 
-		element(by.buttonText('Log me in')).click();
-		protractor.getInstance().sleep(250);
+			element(by.buttonText('Log me in')).click();
+			browser.driver.sleep(250);
+			followUp();
+		});
+
+		
 	}
 	
 	it('Login / Logout', function() {
 				
-		login();
-				
-		element(by.linkText('Log out')).click();
+		login(function () {
+			element(by.linkText('Log out')).click();	
+		});
 
-	});
+	});	
 	
 	it('Create location', function() {
 		
 		var locName = chance.word({syllables: 5});
 		
-		login();
+		login(function () {
+			element(by.linkText('Add')).click();		
 		
-		element(by.linkText('Add')).click();		
-		
-		element(by.model('data.officialName')).sendKeys(locName)
-		element(by.model('data.address')).sendKeys("Alexanderplatz 1")
-		element(by.model('data.city')).sendKeys("Berlin")
-				
-		element(by.buttonText('Save')).click();
-		
-		browser.get('http://localhost:8080/#/updates');
-		
-		element.all(by.repeater('update in rows').column('update.text')).filter(function(elem){
-			return elem.getText().then(function(text) {
-				return text === 'New location '+locName+' in Berlin';
+			element(by.model('data.officialName')).sendKeys(locName)
+			element(by.model('data.address')).sendKeys("Alexanderplatz 1")
+			element(by.model('data.city')).sendKeys("Berlin")
+					
+			element(by.buttonText('Save')).click();
+			
+			browser.get('http://localhost:8080/#/updates');
+			
+			element.all(by.repeater('update in rows').column('update.text')).filter(function(elem){
+				return elem.getText().then(function(text) {
+					return text === 'New location '+locName+' in Berlin';
+				});
+			}).then(function(filteredList) {
+				expect(filteredList.length).toBe(1);	
 			});
-		}).then(function(filteredList) {
-			expect(filteredList.length).toBe(1);	
 		});
+		
+		
 	});
+	
 });
